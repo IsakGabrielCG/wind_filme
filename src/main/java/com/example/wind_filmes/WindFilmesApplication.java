1a5215ab44c5145cf2b0ddfe7998d3a66778a20b
Isak Gabriel Chedid Girardello <isakgabrielcg@gmail.com>
2025-10-08 20:36:48 -0300

    Initial commit

---
+++ src/main/java/com/example/wind_filmes/WindFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.example.wind_filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wind_filmes/WindFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wind_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
