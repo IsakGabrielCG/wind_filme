af689c79875a8d3067a263ecc36d6af4574e070f
Alexandre <alexandrevc4@gmail.com>
2025-10-08 21:30:57 -0300

    created the categories.

--- src/main/java/com/example/wind_filmes/entity/Category.java
+++ src/main/java/com/example/wind_filmes/entity/Category.java
@@ -1,4 +1,25 @@
 package com.example.wind_filmes.entity;
 
-public class Category {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "categories")
+public class Category  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
