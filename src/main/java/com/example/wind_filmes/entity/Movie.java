18699fd37367deb9660a9590ece1de94757e3ac8
Alexandre <alexandrevc4@gmail.com>
2025-10-09 21:00:09 -0300

    Adicionado compos da classe Movie

--- src/main/java/com/example/wind_filmes/entity/Movie.java
+++ src/main/java/com/example/wind_filmes/entity/Movie.java
@@ -1,4 +1,20 @@
 package com.example.wind_filmes.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "movie")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String year; // ano mes dia
+    private Integer old;
+    private String description;
+    private String duracao;
 }
